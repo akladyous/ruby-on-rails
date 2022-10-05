85218132b55717629ec735d77559c3ee31bd8806
Boula Akladyous <akladyous@gmail.com>
2022-10-05 17:25:07 -0400

    added flash partial & bootstrap_flash helper

--- practice-four/app/helpers/application_helper.rb
+++ practice-four/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+  def bootstrap_class_for(flash_type)
+    classes = {
+      success:  'alert alert-success alert-dismissible fade show',
+      error:    'alert alert-danger alert-dismissible fade show',
+      alert:    'alert alert-warning alert-dismissible fade show',
+      notice:   'alert alert-info alert-dismissible fade show'
+    }
+    classes[flash_type.to_sym] || classes[:notice]
+  end
 end
