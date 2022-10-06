4c3285e4e841e75cdcf9500cf23a2ee97a53ce23
Boula Akladyous <akladyous@gmail.com>
2022-10-06 10:05:17 -0400

    added links partial

--- practice-four/app/controllers/posts_controller.rb
+++ practice-four/app/controllers/posts_controller.rb
@@ -1,2 +1,57 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy ]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_path(@post), success: 'Post successfully created'}
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_path(@post), notice: 'post successfully updated' }
+      else
+        format.html { render :edit, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require[:post].permit(:title, :content)
+  end
+
+
 end
