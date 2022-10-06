379c8dbe69f8162ad14ae1db0b80219e5cba302a
Boula Akladyous <akladyous@gmail.com>
2022-10-06 01:12:09 -0400

    create post model & post controller

---
+++ practice-four/app/models/post.rb
@@ -0,0 +1,56 @@
+class Post < ApplicationRecord
+  before_action :set_posts, only: [:show, :edit, :update, :destroy ]
+
+  def index
+    @posts = Post.all.order(create_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_path(@post), success: 'Post successfully created'}
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_path(@post), notice: 'post successfully updated' }
+      else
+        format.html { render :edit, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+    end
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require[:post].permit(:title, :content)
+  end
+
+  private set_post, post_params
+
+end
