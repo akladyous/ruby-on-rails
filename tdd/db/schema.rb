83b0caa309a9ff33eb311925b107457237713509
Boula Akladyous <akladyous@gmail.com>
2022-10-21 12:18:57 -0400

    generate user scaffold for TDD project

--- tdd/db/schema.rb
+++ tdd/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_135604) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_161706) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
