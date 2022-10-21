6e4f4b13f055c8cf3fba2038cc43de4fe38488be
Boula Akladyous <akladyous@gmail.com>
2022-10-21 14:15:42 -0400

    added rails-controller-testing gem

--- tdd/spec/controllers/users_spec.rb
+++ tdd/spec/controllers/users_spec.rb
@@ -1,20 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
   after(:all) do
     User.delete_all
   end
   context "GET #index" do
+    before(:all) do
+      User.create!(first_name: "first", last_name: 'last', email: "sample1@example.com")
+      User.create!(first_name: "first", last_name: 'last', email: "sample2@example.com")
+      User.create!(first_name: "first", last_name: 'last', email: "sample3@example.com")
+    end
+    let(:users) { User.all }
+
     it "return a success response" do
       # expect(response).to be_successful
       expect(response).to have_http_status(:success)
     end
-    xit "render @index template" do
-      expect(response).to render_template(:index)
+    it "assigns all users to @users" do
+      get :index
+      expect(assigns['users']).to eq(users)
+    end
+    it "render @index template" do
+      get :index
+      expect(response).to render_template('index')
     end
   end
 
-  context "SHOW #index" do
+  context "GET #show" do
     it "return a success response" do
       user = User.create!(first_name: "first", last_name: 'last', email: "sample@example.com")
       get :show, params: { id: user.to_param }
