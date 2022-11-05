c9c72d46dee488aab3fbafe239c6d7ce82c9b441
Boula Akladyous <akladyous@gmail.com>
2022-11-05 18:19:01 -0400

    added gem graphiql-rails

--- apiapp/config/routes.rb
+++ apiapp/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
 end
