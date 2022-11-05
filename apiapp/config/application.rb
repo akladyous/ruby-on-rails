b33b7f9b73c0f49aeb11703d7183146ac574ae1c
Boula Akladyous <akladyous@gmail.com>
2022-11-05 18:27:15 -0400

    added graphql-rails

--- apiapp/config/application.rb
+++ apiapp/config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+require "sprockets/railtie"
 module Apiapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -23,5 +23,7 @@ module Apiapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
   end
 end
