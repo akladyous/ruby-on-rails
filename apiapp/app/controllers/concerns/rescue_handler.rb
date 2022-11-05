6a34336818ff89b15fc82e04198cac51b7caecdf
Boula Akladyous <akladyous@gmail.com>
2022-11-05 10:20:05 -0400

    apiapp

--- apiapp/app/controllers/concerns/rescue_handler.rb
+++ apiapp/app/controllers/concerns/rescue_handler.rb
@@ -1,6 +1,17 @@
 module RescueHandler
 
+  extend ActiveSupport::Concern
+  included do
+    rescue_from ActiveRecord::RecordInvalid do |e|
 
+    end
+    rescue_from ActiveRecord::RecordNotFound do |e|
 
+    end
 
+    rescue_from StandardError do |e|
+
+    end
+
+  end
 end
