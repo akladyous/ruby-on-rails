af3b57475484f5d16991836b32be8c8efb565f89
Boula Akladyous <akladyous@gmail.com>
2022-11-06 12:52:28 -0500

    graphql

--- apiapp/app/graphql/types/query_type.rb
+++ apiapp/app/graphql/types/query_type.rb
@@ -7,6 +7,18 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :users,
+          [Types::UserType],
+          null: false,
+          description: 'all users'
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
