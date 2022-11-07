e1a7098b93d9a827eff10ec2fd24c15404595726
Boula Akladyous <akladyous@gmail.com>
2022-11-07 15:30:31 -0500

    practice-one

--- practice-one/db/schema.rb
+++ practice-one/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_202755) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_203016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_202755) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
