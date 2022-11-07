8ba227a4284ca6bed8d068458b1ecd711f4a795b
Boula Akladyous <akladyous@gmail.com>
2022-11-07 14:54:29 -0500

    practice-one

--- practice-one/db/migrate/20221107195054_add_indexes_to_person.rb
+++ practice-one/db/migrate/20221107195054_add_indexes_to_person.rb
@@ -1,6 +1,10 @@
 class AddIndexesToPerson < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_index :people, :first_name
     add_index :people, :last_name
+  end
+  def down
+    remove_index :people, :first_name
+    remove_index :people, :last_name
   end
 end
