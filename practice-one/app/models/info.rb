de46e62dab93771b297b4fc9f23481edfb7d2035
Boula Akladyous <akladyous@gmail.com>
2022-11-08 17:49:16 -0500

    practice-one

--- practice-one/app/models/info.rb
+++ practice-one/app/models/info.rb
@@ -14,3 +14,7 @@ class Info < ApplicationRecord
       end
     end
 end
+# SELECT COUNT(users.id) AS users_count, infos.name FROM infos LEFT OUTER JOIN "users" ON users.id = infos.user_id GROUP BY infos.name;
+# select count(users.id) as user_count, infos.name from infos left outer join users on users.id = infos.id group by infos.name;
+
+# Info.left_outer_joins(:user).group(:name).count("users.id")
