21432108733c5c49b3b65602abc5029f059a946f
Boula Akladyous <akladyous@gmail.com>
2022-11-09 00:08:10 -0500

    practice-one

--- practice-one/app/models/user.rb
+++ practice-one/app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :infos, dependent: :destroy
   has_many :posts
-  # has_many :liked_post, through: :likes, resource: :post
+  has_many :liked_posts, through: :likes, source: :post
+
 
   # has_many :posts, after_add: :capitalize_title
   def capitalize_title(post)
@@ -11,3 +12,6 @@ class User < ApplicationRecord
     # self.title.capitalize
   end
 end
+# SELECT "posts".* FROM "posts"
+# INNER JOIN "likes" ON "posts"."id" = "likes"."post_id"
+# WHERE "likes"."user_id" = $1  [["user_id", 1]]
