b31295ce0357023a52ae6114a846a237234b4cd7
Boula Akladyous <akladyous@gmail.com>
2022-11-12 13:39:21 -0500

    practice-one

--- practice-one/app/controllers/errors_controller.rb
+++ practice-one/app/controllers/errors_controller.rb
@@ -1,13 +1,15 @@
 class ErrorsController < ApplicationController
   def show
-    debugger
     @exception = request.env["action_dispatch.exception"]
     @status_code = ActionDispatch::ExceptionWrapper.status_code_for_exception(@exception.class.name)
-    render file: "#{Rails.root}/public/#{request.path[1..-1]}.html", template: false
-
-    private
-    def view_for_status_code(statuscode)
-      "#{Rails.root}/public/#{request.path[1..-1]}.html"
+    respond_to do |format|
+      # format.html { render file: "#{Rails.root}/public/#{request.path[1..-1]}.html" #, template: false }
+      format.html { render action: "#{request.path[1..-1]}" } #, template: false }
     end
+  end
+
+  private
+  def view_for_status_code(statuscode)
+    "#{Rails.root}/public/#{request.path[1..-1]}.html"
   end
 end
