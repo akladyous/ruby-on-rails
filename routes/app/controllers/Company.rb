64cf68a61cfcede2251948f7ec3d30da345cd49a
Boula Akladyous <akladyous@gmail.com>
2023-04-13 17:19:29 -0400

    Untracked files on routes

---
+++ routes/app/controllers/Company.rb
@@ -0,0 +1,4 @@
+module Company
+
+
+end
--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -1,6 +1,35 @@
 Rails.application.routes.draw do
+  resources :users
   # root 'home#index'
-  get("/", controller: :home, action: :index)
 
-  match("/test:id", to: "home#index", via: :get)
+  # get("/", controller: :home, action: :index)
+
+  # Options ----------------
+  # :path
+  # :controller
+  # :action
+  # :param
+  # :module
+  # :as
+  # :via
+  # :to
+  # :on
+  # :constraints
+  # :defaults
+  # :format
+
+
+  match 'photos/:id' => 'home#show', via: :get
+  match 'photos/:id', to: 'home#show', via: :post
+  match 'photos/:id', controller: 'home', action: 'index', via: :delete
+
+  match 'one', to: "home#index", via: :get
+                                # via: [:get, :post]
+                                # via: :all
+
+  match 'two/(:id)', controller: :home, action: :index, via: :all
+  # match("/test:(id)", to: "home#index", via: :get)
+  # match "home/:id", module: :Company
+
+
 end
