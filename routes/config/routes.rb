7f23af658ec29c11e102f15eff4b47916ebf7e90
Boula Akladyous <akladyous@gmail.com>
2023-04-15 11:48:16 -0400

    Untracked files on routes

--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -1,37 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  # get("/", controller: :home, action: :index)
-
-  # get 'students/:id', to: 'students#show', as: 'grades'
-  # get "students/:id", controller: :students, action: 'show'
-
-
-
-      # ActionDispatch::Routing::Mapper::Base
-  # Options ----------------
-  # :path
-  # :controller
-  # :action
-  # :param
-  # :path
-  # :module
-  # :as
-  # :via
-  # :to
-  # :on
-  # :constraints
-  # :defaults
-  # :format
-  # match(path, options = nil) ⇒ Object
-  match "test",       to: "test#index", via: :get
-  match "test",       to: "test#index", via: :delete, param: :test_id
-  match 'test/:id'    =>  'test#show', via: :get, as: "get_photos"
-  match 'test/:id',   to: 'test#show', via: :post
-  match 'test/:id',   to: 'test#show', via: :patch
-  match 'test/:id',   controller: 'photos', action: 'index', via: :all
-  match 'test/:id',   to: 'test#update', constraints: {format: 'json'}, via: :get
-  match 'test/:id',   to: 'test#delete', constraints: {id: /[A-Z]\d{3}/}, via: :get
-  match 'test',       to: 'test#index', defaults: {format: 'jpg'}, via: :get
 
   # RESOURCES < ----------------------------------------
   # Options : Takes same options as 'match' as well as:
@@ -47,13 +15,13 @@ Rails.application.routes.draw do
 
   # get "students/:id", to: "students#show", constraints: {id: /\d{1,2}/}
   # constraints(id: /\d{1,3}/) do
-    resources :students, path: 'studenti' do #, path_names: {update: 'student_path'} do
-      # member do
-      #   patch :publish
-      # end
-      # collection do
-      #   delete :publish_all
-      # end
+    resources :students, path: 'studenti', path_names: {update: 'student_path'} do
+      member do
+        patch :publish
+      end
+      collection do
+        delete :publish_all
+      end
     end
   # end
 
