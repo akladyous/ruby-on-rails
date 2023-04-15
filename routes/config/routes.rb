44d7e5852058dba279305eae34ae5f5ad3951002
Boula Akladyous <akladyous@gmail.com>
2023-04-15 12:00:48 -0400

    Untracked files on routes

--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -1,6 +1,42 @@
 Rails.application.routes.draw do
   root 'home#index'
-  draw(:singular_routes)
+  # draw(:singular_routes) -- including all routes defined in config/routes/singular_routes.rb
+  # get("/", controller: :home, action: :index)
+
+  # get 'students/:id', to: 'students#show', as: 'grades'
+  # get "students/:id", controller: :students, action: 'show'
+
+
+
+  # ActionDispatch::Routing::Mapper::Base
+  # Options ----------------
+  # :path
+  # :controller
+  # :action
+  # :param
+  # :path
+  # :module
+  # :as
+  # :via
+  # :to
+  # :on
+  # :constraints
+  # :defaults
+  # :format
+  # match(path, options = nil) ⇒ Object
+  match "test",       to: "test#index", via: :get
+  match "test",       to: "test#index", via: :delete, param: :test_id
+  match 'test/:id'    =>  'test#show', via: :get, as: "get_photos"
+  match 'test/:id',   to: 'test#show', via: :post
+  match 'test/:id',   to: 'test#show', via: :patch
+  match 'test/:id',   controller: 'photos', action: 'index', via: :all
+  match 'test/:id',   to: 'test#update', constraints: {format: 'json'}, via: :get
+  match 'test/:id',   to: 'test#delete', constraints: {id: /[A-Z]\d{3}/}, via: :get
+  match 'test',       to: 'test#index', defaults: {format: 'jpg'}, via: :get
+  get   '/test/:name',to: redirect('/articles/%{name}', status: 302)
+  post  'test/:id', to: -> { || }
+
+
 
   # RESOURCES < ----------------------------------------
   # Options : Takes same options as 'match' as well as:
