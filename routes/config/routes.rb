89590457d3573b05e10d8ee24c6d87a32b588a52
Boula Akladyous <akladyous@gmail.com>
2023-04-26 12:11:52 -0400

    Untracked files on routes

--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   # get 'students/:id', to: 'students#show', as: 'grades'
   # get "students/:id", controller: :students, action: 'show'
 
-
-
   # ActionDispatch::Routing::Mapper::Base
   # Options ----------------
   # :path
@@ -24,18 +22,17 @@ Rails.application.routes.draw do
   # :defaults
   # :format
   # match(path, options = nil) ⇒ Object
-  match "test",       to: "test#index", via: :get
-  match "test",       to: "test#index", via: :delete, param: :test_id
-  match 'test/:id'    =>  'test#show', via: :get, as: "get_photos"
+  match 'test',       to: 'test#index', via: :get
+  match 'test',       to: 'test#index', via: :delete, param: :test_id
+  match 'test/:id' => 'test#show', via: :get, as: 'get_photos'
   match 'test/:id',   to: 'test#show', via: :post
   match 'test/:id',   to: 'test#show', via: :patch
   match 'test/:id',   controller: 'photos', action: 'index', via: :all
-  match 'test/:id',   to: 'test#update', constraints: {format: 'json'}, via: :get
-  match 'test/:id',   to: 'test#delete', constraints: {id: /[A-Z]\d{3}/}, via: :get
-  match 'test',       to: 'test#index', defaults: {format: 'jpg'}, via: :get
-  get   '/test/:name',to: redirect('/articles/%{name}', status: 302)
-  post  'test/:id', to: -> {  }
-
+  match 'test/:id',   to: 'test#update', constraints: { format: 'json' }, via: :get
+  match 'test/:id',   to: 'test#delete', constraints: { id: /[A-Z]\d{3}/ }, via: :get
+  match 'test',       to: 'test#index', defaults: { format: 'jpg' }, via: :get
+  get   '/test/:name', to: redirect('/articles/%<name>s', status: 302)
+  post  'test/:id', to: -> {}
 
   # RESOURCES < ----------------------------------------
   # Options : Takes same options as 'match' as well as:
@@ -48,28 +45,27 @@ Rails.application.routes.draw do
   # :format
   # :param
 
-
   # get "students/:id", to: "students#show", constraints: {id: /\d{1,2}/}
   # constraints(id: /\d{1,3}/) do
-    resources :students, path: 'studenti', path_names: {update: 'student_path'} do
-      member do
-        patch :publish
-      end
-      collection do
-        delete :publish_all
-      end
+  resources :students, path: 'studenti', path_names: { update: 'student_path' } do
+    member do
+      patch :publish
     end
+    collection do
+      delete :publish_all
+    end
+  end
   # end
 
-  resources :users, shallow: true  do
-        resources :photos, param: :photo_id, path: 'images'
+  resources :users, shallow: true do
+    resources :photos, param: :photo_id, path: 'images'
   end
 
   resources :animals, shallow: true do
     resources :dogs
   end
 
-  resources :posts, path: "posting" do
+  resources :posts, path: 'posting' do
     member do
       get 'member'
       post 'member'
@@ -80,15 +76,11 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace "admin", module: 'prova' do
+  namespace 'admin', module: 'prova' do
     resources :books
   end
 
-  scope  module: 'blog' do
+  scope module: 'blog' do
     resources :posts
   end
 end
-
-
-
-
